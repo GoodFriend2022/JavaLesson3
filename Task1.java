dca071416ff165303e8d98de46226884cab2ad03
Uri_Klevcov <a-08-137.mail.ru>
2023-02-19 17:53:52 +0600

    FillArray

--- Task1.java
+++ Task1.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка.
 public class Task1 {
     public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        FillArray(numbers, 10, 0, 20);
+        System.out.println(numbers);
         
+    }
+    static void FillArray(ArrayList array, int lengthArray, int minNumb, int maxNumb) {
+        Random numb = new Random();
+        for (int i = 1; i < lengthArray; i++) {
+            array.add(numb.nextInt(minNumb, maxNumb));
+        }
+    }
+
+    static int FindMinNumb(ArrayList array) {
+        int min = array.get(0);
+        for (int i = 1; i < array.length; i++) {
+            
+        }
     }
 }
