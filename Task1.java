b75cf586849320efdecb661b71074151172b0e33
Uri_Klevcov <a-08-137.mail.ru>
2023-02-19 18:22:49 +0600

    Task1

--- Task1.java
+++ Task1.java
@@ -7,19 +7,43 @@ public class Task1 {
         ArrayList<Integer> numbers = new ArrayList<>();
         FillArray(numbers, 10, 0, 20);
         System.out.println(numbers);
+        int minEl = FindMinNumb(numbers);
+        int maxEl = FindMaxNumb(numbers);
+        double sumArray = SumElArray(numbers);
+        double lengthAr = numbers.size();
+        double aver = sumArray / lengthAr;
+        System.out.printf(" Минимальный элемент - %d;\n Максимальный элемент - %d;\n "
+            + "Среднее арифметическое - %.1f\n", minEl, maxEl, aver);
         
     }
-    static void FillArray(ArrayList array, int lengthArray, int minNumb, int maxNumb) {
+    static void FillArray(ArrayList<Integer> array, int lengthArray, int minNumb, int maxNumb) {
         Random numb = new Random();
-        for (int i = 1; i < lengthArray; i++) {
+        for (int i = 0; i < lengthArray; i++) {
             array.add(numb.nextInt(minNumb, maxNumb));
         }
     }
 
-    static int FindMinNumb(ArrayList array) {
-        int min = array.get(0);
-        for (int i = 1; i < array.length; i++) {
-            
+    static int FindMinNumb(ArrayList<Integer> array) {
+        int min = (int) array.get(0);
+        for (int i = 1; i < array.size(); i++) {
+            if (min > array.get(i)) min = array.get(i);
         }
+        return min;
+    }
+
+    static int FindMaxNumb(ArrayList<Integer> array) {
+        int max = (int) array.get(0);
+        for (int i = 1; i < array.size(); i++) {
+            if (max < array.get(i)) max = array.get(i);
+        }
+        return max;
+    }
+
+    static int SumElArray(ArrayList<Integer> array) {
+        int sum = 0;
+        for (int i = 0; i < array.size(); i++) {
+            sum +=array.get(i);
+        }
+        return sum;
     }
 }
